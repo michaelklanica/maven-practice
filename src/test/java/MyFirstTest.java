c9d74cb2b8c85ecae1110d6c0683d73c6540ee55
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-20 19:13:43 -0500

    first commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+
+
+
+
+
+}
