c351186bbb5711f1d56ab6ba8b8b6b2e470b11a3
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-21 09:26:44 -0500

    JUnit Testing exercise

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,8 +1,12 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
+
     @Test
     public void testIfCompanyNameIsEquals() {
         String expected = "Codeup";
@@ -10,44 +14,34 @@ public class MyFirstTest {
 
         assertEquals(expected, actual);
     }
-// When you're using floating points values a third parameter is required (Delta) which represents a positive margin of error of decimals.
-
-    @Test
-    public void testIfChangeIsCorrect() {
-        Double price = 10.0;
-        Double discount = 4.5;
-
-        assertEquals(5.5, price - discount, 0);
-        assertEquals(5.1, price - discount, 0.5);
-        assertNotEquals(4.9, price - discount, 0.5);
-    }
 
     @Test
     public void testIfObjectsAreDifferent() {
-        Object dog = new Object();
-        Object sheep = new Object();
-        Object clonnedSheep = sheep;
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
 
-        assertNotSame(sheep, dog);
-        assertSame(sheep, clonnedSheep);
+        assertNotSame(languages, moreLanguages);
+
     }
 
     @Test
     public void testIfArrayEquals() {
-        char[] expected = {'J','u','n','i','t'};
-        char[] actual = "Junit".toCharArray();
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
 
-        assertArrayEquals(expected, actual);
+        assertArrayEquals(numbers, otherNumbers);
     }
 
     @Test
-    public void testIfGreaterOrLesserThanWorks() {
+    public void testIfStringContainsChar() {
 
-        boolean learningTDD = true;
+        String language = "PHP";
 
-        assertTrue(learningTDD);
-        assertTrue("5 is greater than 4", 5 > 4);
-        assertFalse("5 is not greater than 6", 5 > 6);
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
     }
 
 
